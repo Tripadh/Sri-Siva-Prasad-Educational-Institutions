d3edb51fc1e53ea83a34fd72d9ac2720a1e780ae
2400032045 <tripadhv@gmail.com>
2025-10-12 17:42:34 +0530

    Updated favicon and index.html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -237,6 +237,9 @@ export default function Home() {
             <div className="carousel-item">
               <img src={slide5} className="d-block w-100" alt="Fifth Slide" />
             </div>
+            <div className="carousel-item">
+              <img src={slide6} className="d-block w-100" alt="Fifth Slide" />
+            </div>
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true" />
